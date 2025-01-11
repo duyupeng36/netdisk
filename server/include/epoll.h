4b9f7cacdb4eb37bf72771b3f8601706e535bd9d
杜宇鹏 <duyupeng36@foxmail.com>
2025-01-11 18:44:13 +0800

    服务端：接入 EPOLL

---
+++ server/include/epoll.h
@@ -0,0 +1,26 @@
+#ifndef EPOLL_H
+#define EPOLL_H
+#include <sys/types.h>
+#include <sys/epoll.h>
+
+/**
+ * @brief Add fd to the epoll instance to listen for events events
+ * 
+ * @param epfd File descriptor for the epoll instance
+ * @param fd  The file descriptor to add
+ * @param events Events to listen fo
+ * @return on success return 0, otherwise return -1
+ */
+int epoll_add(int epfd, int fd, uint32_t events);
+
+/**
+ * @brief delete fd from the epoll instance
+ * 
+ * @param epfd File descriptor for the epoll instance
+ * @param fd The file descriptor to delete
+ * @return on success return 0, otherwise return -1
+ */
+int epoll_del(int epfd, int fd);
+
+
+#endif
--- server/main.c
+++ server/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include <sys/sysinfo.h>
 #include <sys/socket.h>
@@ -8,6 +9,7 @@
 
 #include "config.h"
 #include "tcp.h"
+#include "epoll.h"
 
 #define CONFIG_PATH "config/config.toml"
 
@@ -21,6 +23,7 @@ int main(int argc, char *argv[]) {
 
     char *config_path = argc > 2 ? argv[1] : CONFIG_PATH;
 
+    // 读取服务端配置文件
     struct server_config server_config;
     server_config.hostname = malloc(sizeof(char) * 16);
     server_config.service = malloc(sizeof(char) * 6); 
@@ -29,12 +32,78 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // 创建线程池
+
+
+    // 创建监听套接字
     int sockfd = tcp_listen(server_config.hostname, server_config.service);
     if(sockfd == -1) {
         fprintf(stderr, "Failed to create socket\n");
         return 1;
     }
+
+    // 创建 epoll 实例
+    int epfd = epoll_create(1024);
+    if(epfd == -1) {
+        fprintf(stderr, "Failed to create epoll\n");
+        return 1;
+    }
+
+    // 添加监听套接字到 epoll 实例
+    if(epoll_add(epfd, sockfd, EPOLLIN) == -1) {
+        fprintf(stderr, "Failed to add sockfd to epoll\n");
+        return 1;
+    }
+
+    // 监控文件描述符个数
+    int monitorNumber = 1;
+    // 就像数组大小
+    int readySize = 2 * monitorNumber;
+    // 创建就绪时间数组
+    struct epoll_event *events = malloc(sizeof(struct epoll_event) * readySize);
+
+    while (true)
+    {
+        int readyNumber = epoll_wait(epfd, events, 2 * monitorNumber, -1);
+        if(readyNumber == -1) {
+            fprintf(stderr, "Failed to wait for epoll\n");
+            return 1;
+        }
+
+        // 循环处理事件
+        for(int i = 0; i < readyNumber; i++) {
+            if(events[i].data.fd == sockfd) {
+                // 有新的客户端连接
+                int netfd = accept(events[i].data.fd, NULL, NULL);
+                if(netfd == -1) {
+                    fprintf(stderr, "Failed to accept\n");
+                    return 1;
+                }
+                // 添加新的客户端连接到 epoll 实例
+                if(epoll_add(epfd, netfd, EPOLLIN) == -1) {
+                    fprintf(stderr, "Failed to add netfd to epoll\n");
+                    return 1;
+                }
+                monitorNumber++;
+                if(monitorNumber > readySize) {
+                    readySize = 2 * monitorNumber;
+                    events = realloc(events, sizeof(struct epoll_event) * readySize);
+                    if(events == NULL) {
+                        fprintf(stderr, "Failed to realloc events\n");
+                        return 1;
+                    }
+                }
+            } else {
+                // 客户端发来命令
+            }
+        }
+
+    }
     
+
+
+
+
     return 0;
 }
 
